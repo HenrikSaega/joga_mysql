1f27fcbdcbc7259d9ba723b7158e562ae42e8509
Henrik <Henriksaega@gmail.com>
2025-01-31 11:39:01 +0200

    Display all articles

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const app = require('./utils/app');
 const db = require('./utils/db');
+const articleRoutes = require('./routes/articles');
+
+app.use('/', articleRoutes)
 
 app.listen(3012, () => {
-    console.log("Ühendub veebiserveriga!");
+    console.log("Ühendatud veebiserveriga!");
 })
