df85e220c3b47d79ba995f06a7f12d73d7892474
Henrik <Henriksaega@gmail.com>
2025-02-07 11:00:17 +0200

    Add author name into single article

--- controllers/articles.js
+++ controllers/articles.js
@@ -12,8 +12,16 @@ const getAllArticles = (req, res) => {
 const getArticleSlug = (req, res) =>{
     let sql=`SELECT * FROM article WHERE slug="${req.params.slug}"`
     db.query(sql, (error, result) => {
-        res.render('article', {
-            article: result
+        const article = result[0]
+        const author_id = result[0].author_id
+        let sql = `SELECT * FROM author WHERE id="${author_id}"`
+        db.query(sql, (error, result) => {
+            const author= result[0]
+            article['author_name'] = author.name
+            console.log(result)
+            res.render('article', {
+                article: article
+            })
         })
     })
 }
